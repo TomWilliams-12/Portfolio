5f956aa38fe0a1bc5a36655db92a5b33a3623da8
Tom Williams <williams.t17@gmail.com>
2020-07-16 19:00:58 +0100

    menu changes

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const button = document.querySelector(".toggle");
 const menuItems = document.querySelectorAll(".menu-item");
+const mobileMenu = document.querySelector(".menu");
 
 
 function classToggle() {
     menuItems.forEach(item => item.classList.toggle('active'));
+    mobileMenu.classList.toggle('active');
 }
 
 button.addEventListener("click", classToggle);
